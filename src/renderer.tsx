144a311c2766a70f1bfaa35d404d0a6babdbd2b2
Terry <168416857+pdahd@users.noreply.github.com>
2025-02-05 13:35:02 +0800

    Update renderer.tsx

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -53,7 +53,7 @@ export const renderer = jsxRenderer(({ children, title }) => {
             /* 可选：标题和内置函数等高亮颜色 */
             .hljs-title,
             .hljs-built_in {
-              color: #FF00FF !important; /* 绿色 */
+              color: #FF00CC !important; /* 绿色 */
             }
           `}
         </style>
